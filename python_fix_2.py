c160ba62bebadeb0a776377c1f283b8363dcb55e
Tee Chur <teachermetz2@gmail.com>
2025-02-26 19:17:28 +0000

    adding python problems

---
+++ python_fix_2.py
@@ -0,0 +1,10 @@
+def add_numbers(a, b)
+    result = a + b
+    return result
+
+def subtract_numbers(a, b):
+    result = a - b
+    return result
+
+print(add_numbers(5, 3)
+print(subtract_numbers(10, 4)
